c3b6ccad9c5a2f321f515fea6c4fa5de0e224320
devWRM <mcwen@comcast.net>
2021-02-26 10:27:37 -0500

    created seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,5 +10,15 @@
 Subject.destroy_all
 Word.destroy_all
 
+Subject.create(name: "bank")
+Word.create(spelling: "deposit", pos: "verb", definition: "put money in a bank account", sentences: "I want to deposit this check.", subject_id: 1)
+Word.create(spelling: "withdraw", pos: "verb", definition: "take money out of a bank account", sentences: "They withdraw $700 every week.", subject_id: 1)
+
+Subject.create(name: "dinner")
+Word.create(spelling: "eat out", pos: "verb", definition: "have a meal away from home", sentences: "We eat out on Fridays.", subject_id: 2)
+Word.create(spelling: "cook", pos: "verb", definition: "prepare food using heat", sentences: "I like to cook healthy meals", subject_id: 2)
+
+
+
 
 
