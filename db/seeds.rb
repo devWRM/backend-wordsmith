06e5690b56b8fb843de0efcc11aeb7e2636b5015
devWRM <mcwen@comcast.net>
2021-02-26 06:15:15 -0500

    added destroy methods in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Subject.destroy_all
+Word.destroy_all
+
+
+
