79961f03cf484eec79328adbd7c3f0552ba1c0fa
devWRM <mcwen@comcast.net>
2021-04-21 18:16:27 -0400

    created word_check method to check if word is already in user's subject list of words

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -3,4 +3,9 @@ class Subject < ApplicationRecord
     has_many :words, :dependent => :destroy
     validates :name, presence: true
 
+    def word_check(word)
+        return self.words.include?(word)
+    end
+
+
 end
