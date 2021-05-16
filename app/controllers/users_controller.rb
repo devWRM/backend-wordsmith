0a6822f1ad95b586c86b35cff9a3e987914c64a0
devWRM <mcwen@comcast.net>
2021-05-16 14:17:34 -0400

    added new, create, show actions at the top of file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,31 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
 
+
+  def new
+
+  end
+
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+
+  # GET /users/1
+  def show
+    render json: @user
+  end
+
+###################################################################
+
   # GET /users
   def index
     @users = User.all
