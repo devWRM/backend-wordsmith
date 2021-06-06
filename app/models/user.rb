819d62d7454b7208ad81eec0e07f8ef22a2ee8db
devWRM <mcwen@comcast.net>
2021-06-06 15:42:31 -0400

    commented out subjects & words relationships to fix app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    has_many :subjects
-    has_many :words, through: :subjects
+    # has_many :subjects
+    # has_many :words, through: :subjects
 
     has_secure_password
     validates :name, :email, uniqueness: true, presence: true
